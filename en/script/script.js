47f142f4f5e3fdf944f87bdc2dfd9e2a2d297685
azamusaev95 <azamatamerika2@gmail.com>
2023-02-11 12:22:01 +0600

    dfdf

---
+++ en/script/script.js
@@ -0,0 +1,79 @@
+const whatsappCourseContainer = document.getElementById(
+  "whatsappCourseContainer"
+);
+const lessonSix = document.getElementById("lessonSix");
+const lessonSixImg = document.getElementById("lessonSixImg");
+const lessonSeven = document.getElementById("lessonSeven");
+const lessonSevenImg = document.getElementById("lessonSevenImg");
+const lessonEight = document.getElementById("lessonEight");
+const lessonEigthImg = document.getElementById("lessonEigthImg");
+
+const lessonNine = document.getElementById("lessonNine");
+const lessonNineImg = document.getElementById("lessonNineImg");
+const lessonEleven = document.getElementById("lessonEleven");
+const lessonElevenImg = document.getElementById("lessonElevenImg");
+const lessonTwenty = document.getElementById("lessonTwenty");
+const lessonTwentyImg = document.getElementById("lessonTwentyImg");
+const linkStatusShow = document.createElement("p");
+
+const createLocalStorage = (boolean) => {
+  localStorage.setItem("WhatsappCourseCheck", JSON.stringify(boolean));
+  return boolean;
+};
+console.log(whatsappCourseContainer);
+const localStorageCourseValue =
+  localStorage.getItem("WhatsappCourseCheck") === null
+    ? createLocalStorage(false)
+    : JSON.parse(localStorage.getItem("WhatsappCourseCheck"));
+
+console.log("localStorageCourseValue", localStorageCourseValue);
+
+let count = 4;
+linkStatusShow.innerHTML = `Сабактар ачылуусуна ${count} бөлүшүү калды`;
+const linkFunction = (link) => {
+  if (count === 0) {
+    const shareButton = document.getElementById("shareButton");
+
+    const shareButtonLink = document.getElementById("shareButtonLink");
+    shareButtonLink.setAttribute("href", link);
+    createLocalStorage(true);
+    shareButtonLink.setAttribute("href", "../index.htm");
+    shareButton.innerText = "Башкы бетке отуу";
+  } else if (count === 1) {
+    shareButton.innerText = "Башкы бетке отуу";
+    const shareButtonLink = document.getElementById("shareButtonLink");
+  }
+  lessonsStatusCheker();
+  count--;
+  console.log(count);
+  linkStatusShow.innerHTML = `Cабактар ачылуусуна ${count} бөлүшүү калды`;
+};
+
+const lessonsStatusCheker = () => {
+  console.log(localStorageCourseValue);
+  if (localStorageCourseValue === true) {
+    console.log("ssss");
+    try {
+      lessonSix.setAttribute("href", "urok6/index.htm");
+      lessonSixImg.setAttribute("href", "urok6/index.htm");
+      lessonSeven.setAttribute("href", "urok7/index.htm");
+      lessonSevenImg.setAttribute("href", "urok7/index.htm");
+      lessonEight.setAttribute("href", "urok8/index.htm");
+      lessonEigthImg.setAttribute("href", "urok8/index.htm");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+lessonsStatusCheker();
+console.log(whatsappCourseContainer);
+whatsappCourseContainer.append(linkStatusShow);
+
+// const localStorageMektepValue =
+//   localStorage.getItem("WhatsappMektepCheck") === null
+//     ? localStorage.setItem(JSON.stringify("WhatsappMektepCheck", false))
+//     : localStorage.getItem(localStorage.getItem("WhatsappMektepCheck"));
+
+// if (!localStorageMektepValue) {
+// }
---
+++ en/script2/script2.js
@@ -0,0 +1,73 @@
+const whatsappCourseContainer = document.getElementById(
+  "whatsappMektepContainer"
+);
+const lessonSix = document.getElementById("lessonSix");
+const lessonSixImg = document.getElementById("lessonSixImg");
+const lessonSeven = document.getElementById("lessonSeven");
+const lessonSevenImg = document.getElementById("lessonSevenImg");
+const lessonEight = document.getElementById("lessonEight");
+const lessonEigthImg = document.getElementById("lessonEigthImg");
+
+const lessonNine = document.getElementById("lessonNine");
+const lessonNineImg = document.getElementById("lessonNineImg");
+const lessonEleven = document.getElementById("lessonEleven");
+const lessonElevenImg = document.getElementById("lessonElevenImg");
+const lessonTwenty = document.getElementById("lessonTwenty");
+const lessonTwentyImg = document.getElementById("lessonTwentyImg");
+const linkStatusShow = document.createElement("p");
+
+const createLocalStorage = (boolean) => {
+  localStorage.setItem("WhatsappMektepCheck", JSON.stringify(boolean));
+  return boolean;
+};
+console.log(whatsappCourseContainer);
+const localStorageCourseValue =
+  localStorage.getItem("WhatsappMektepCheck") === null
+    ? createLocalStorage(false)
+    : JSON.parse(localStorage.getItem("WhatsappMektepCheck"));
+
+console.log("localStorageCourseValue", localStorageCourseValue);
+
+let count = 4;
+linkStatusShow.innerHTML = `Сабактар ачылуусуна ${count} бөлүшүү калды`;
+const linkFunction = (link) => {
+  if (count === 0) {
+    const shareButton = document.getElementById("shareButton");
+
+    const shareButtonLink = document.getElementById("shareButtonLink");
+    shareButtonLink.setAttribute("href", link);
+    createLocalStorage(true);
+    shareButtonLink.setAttribute("href", "../index.htm");
+    shareButton.innerText = "Башкы бетке отуу";
+  } else if (count === 1) {
+    shareButton.innerText = "Башкы бетке отуу";
+    const shareButtonLink = document.getElementById("shareButtonLink");
+  }
+  lessonsStatusCheker();
+  count--;
+  console.log(count);
+  linkStatusShow.innerHTML = `Cабактар ачылуусуна ${count} бөлүшүү калды`;
+};
+
+const lessonsStatusCheker = () => {
+  console.log(localStorageCourseValue);
+  if (localStorageCourseValue === true) {
+    console.log("ssss");
+    try {
+      lessonNine.setAttribute("href", "urok10/index.htm");
+      lessonNineImg.setAttribute("href", "urok10/index.htm");
+      lessonEleven.setAttribute("href", "urok11/index.htm");
+      lessonElevenImg.setAttribute("href", "urok11/index.htm");
+      lessonTwenty.setAttribute("href", "urok12/index.htm");
+      lessonTwentyImg.setAttribute("href", "urok12/index.htm");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+lessonsStatusCheker();
+console.log(whatsappCourseContainer);
+whatsappCourseContainer.append(linkStatusShow);
+
+
